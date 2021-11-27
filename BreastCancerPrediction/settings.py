79a2cba79861c4ebb2e0bc06811e92738b5644a2
Sarthik-mehta <65113721+Sarthik-mehta@users.noreply.github.com>
2021-11-27 21:49:13 +0530

    fifth commit

--- BreastCancerPrediction/settings.py
+++ BreastCancerPrediction/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ihaky$&k2poue7l$nk8w(no$@c4odljmt4rnkb!)&wi$%znb-8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
